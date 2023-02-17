183644596ef825cfcfabf3cf13ecca679d34c04b
sainihimanshu1999 <sainihimanshu.1999@gmail.com>
2023-02-17 17:32:46 +0530

    finished

---
+++ src/NotFound.js
@@ -0,0 +1,14 @@
+const NotFound =() => {
+    return(
+        <header className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+                <div className="col-8">
+                    <h1 className="text-danger">Error 404 </h1>
+                    <h2>Page not found</h2>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default NotFound;
